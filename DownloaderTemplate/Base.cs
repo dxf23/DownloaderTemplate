e4e83f6be189656978780bda000d53666aa5ad85
dxf23 <rufus.monke@gmail.com>
2025-04-21 09:19:27 +0100

    Add files via upload

---
+++ DownloaderTemplate/Base.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using static System.Net.WebRequestMethods;
+
+namespace ConsoleApp1
+{
+    public class Base
+    {
+        static void Main()
+        {
+            d();
+        }
+        static void d()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("1. Download iis Stupid Menu");
+                Console.WriteLine("2. Download Fortnite Emote Wheel");
+                Console.WriteLine("3. Exit the application");
+                Console.WriteLine("-------------------------------");
+                Console.Write("Please enter your choice (1, 2 or 3): ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        iidk();
+                        break;
+                    case "2":
+                        iidk2();
+                        break;
+                    case "3":
+                        exitapp();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Press Enter to try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+        static void iidk()
+        {
+            Console.WriteLine("ok now check the bin folder in the source for Downloader.");
+            using (WebClient client = new WebClient())
+            {
+                string address = "https://github.com/iiDk-the-actual/iis.Stupid.Menu/releases/download/5.9.0/iis_Stupid_Menu.dll";
+                string fileName = "iis Stupid Menu.dll";
+
+                client.DownloadFile(address, fileName);
+            }
+            Console.WriteLine("Press Enter to return to the main menu.");
+            Console.ReadLine();
+        }
+        static void exitapp()
+        {
+            Environment.Exit(0);
+        }
+        static void iidk2()
+        {
+            Console.WriteLine("ok now check the bin folder in the source for DownloaderTemplate.");
+            using (WebClient client = new WebClient())
+            {
+                string address = "https://github.com/iiDk-the-actual/FortniteEmoteWheel/releases/download/1.2.0/FortniteEmoteWheel.dll";
+                string fileName = "FortniteEmoteWheel.dll";
+
+                client.DownloadFile(address, fileName);
+            }
+            Console.WriteLine("Press enter to return to the main menu.");
+            Console.ReadLine();
+        }
+    }
+}

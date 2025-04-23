17b8b39ec0135083081ed4bbda57e8565adfc477
dxf23 <rufus.monke@gmail.com>
2025-04-23 11:42:13 +0100

    Add files via upload

--- DownloaderTemplate/Base.cs
+++ DownloaderTemplate/Base.cs
@@ -18,9 +18,10 @@ namespace ConsoleApp1
                 Console.WriteLine("-------------------------------");
                 Console.WriteLine("1. Download iis Stupid Menu");
                 Console.WriteLine("2. Download Fortnite Emote Wheel");
-                Console.WriteLine("3. Exit the application");
+                Console.WriteLine("3. Download Aura Obfuscator");
+                Console.WriteLine("4. Exit the application");
                 Console.WriteLine("-------------------------------");
-                Console.Write("Please enter your choice (1, 2 or 3): ");
+                Console.Write("Please enter your choice (1, 2, 3 or 4): ");
 
                 string choice = Console.ReadLine();
 
@@ -33,6 +34,9 @@ namespace ConsoleApp1
                         iidk2();
                         break;
                     case "3":
+                        aura();
+                        break;
+                    case "4":
                         exitapp();
                         break;
                     default:
@@ -52,7 +56,7 @@ namespace ConsoleApp1
 
                 client.DownloadFile(address, fileName);
             }
-            Console.WriteLine("Press Enter to return to the main menu.");
+            Console.WriteLine("File download! Press Enter to return to the main menu.");
             Console.ReadLine();
         }
         static void exitapp()
@@ -69,7 +73,20 @@ namespace ConsoleApp1
 
                 client.DownloadFile(address, fileName);
             }
-            Console.WriteLine("Press enter to return to the main menu.");
+            Console.WriteLine("File download! Press enter to return to the main menu.");
+            Console.ReadLine();
+        }
+        static void aura()
+        {
+            Console.WriteLine("ok now check the bin folder in the source for DownloaderTemplate.");
+            using (WebClient client = new WebClient())
+            {
+                string address = "https://github.com/loomisntreal/Aura-.NET-Obfuscator/archive/refs/heads/main.zip";
+                string fileName = "Aura-.NET-Obfuscator-main.zip";
+
+                client.DownloadFile(address, fileName);
+            }
+            Console.WriteLine("File download! Press enter to return to the main menu.");
             Console.ReadLine();
         }
     }
